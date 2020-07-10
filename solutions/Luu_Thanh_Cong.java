f52d761c95551f47ecec8964651ec1a3eda086b2
luuthanhcong <50271940+luuthanhcong@users.noreply.github.com>
2020-07-10 21:13:50 +0700

    Lưu Thành Công nộp b2

--- solutions/Luu_Thanh_Cong.java
+++ solutions/Luu_Thanh_Cong.java
@@ -4,34 +4,24 @@
  * and open the template in the editor.
  */
 package Lession2;
+
 import java.util.Scanner;
 
 /**
  *
  * @author luuthanhcong
  */
-public class B1 {
+public class B2 {
     public static void main(String[] args){
+        // Viết chương trình tính giai thừa của 1 số nguyên dương 
         Scanner s = new Scanner(System.in);
-        System.out.println("Nhập a : ");
-        int a = Integer.parseInt(s.nextLine());
-        System.out.println("Nhap b : ");
-        int b = Integer.parseInt(s.nextLine());
-        System.out.println("Nhap c : ");
-        int c = Integer.parseInt(s.nextLine());
-        
-        float delta = b*b - 4*a*c;
-        float x1 = (float)((-b + Math.sqrt(delta))/(2*a));
-        float x2 = (float) ((-b - Math.sqrt(delta))/(2*a));
-        float x3 = (float)(-b /(2*a));
-        if(delta > 0){
-            System.out.println("Pt co 2 nghiem pb : " + x1 + x2);
-        }else if(delta == 0){
-            System.out.println("Pt co nghiem kep : " + x3);
-        } else {
-            System.out.println("Vo nghiem");
+        System.out.println("Nhập số : ");
+        int n = Integer.parseInt(s.nextLine());
+        int ft = 1;
+        for (int i = 1; i <= n; i++) {
+            ft = ft * i;
         }
-        
+        System.out.println(ft);
     }
-            
+    
 }
